58cd9a9d11477c66cef75e6c68d3415c71f17e08
Xuxian Chen <amberwithmrtraveling@gmail.com>
2018-11-20 10:14:25 -0600

    Add files via upload

---
+++ Chen_Xuxian_HW5/Chen_Xuxian_HW5/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chen_Xuxian_HW5.DAL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chen_Xuxian_HW5
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = "Server=tcp:fa18xuxianchenhw5.database.windows.net,1433;Initial Catalog=fa18XuxianChenHW5;Persist Security Info=False;User ID=fa18hw5XC;Password=123qweASD;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";  //paste your connection string from Azure in between the quotes.
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
