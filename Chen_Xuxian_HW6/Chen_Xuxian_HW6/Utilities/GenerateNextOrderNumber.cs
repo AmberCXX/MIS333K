416f460780ab6f5a62418c86d9e8d69b267248e5
Xuxian Chen <amberwithmrtraveling@gmail.com>
2018-11-20 10:27:19 -0600

    Add files via upload

---
+++ Chen_Xuxian_HW6/Chen_Xuxian_HW6/Utilities/GenerateNextOrderNumber.cs
@@ -0,0 +1,32 @@
+﻿using Chen_Xuxian_HW6.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chen_Xuxian_HW6.Utilities
+{
+    public class GenerateNextOrderNumber
+    {
+        public static Int32 GetNextON(AppDbContext db)
+        {
+            Int32 intMaxON; //the current maximum course number
+            Int32 intNextON; //the course number for the next class
+
+            if (db.Orders.Count() == 0)
+            {
+                intMaxON = 10000;
+            }
+            else
+            {
+                intMaxON = db.Orders.Max(c => c.OrderNumber);
+            }
+
+            //add one to the current max to find the next one
+            intNextON = intMaxON + 1;
+
+            //return the value
+            return intNextON;
+        }
+    }
+}
---
+++ Chen_Xuxian_HW6/Chen_Xuxian_HW6/Utilities/GenerateSKU.cs
@@ -0,0 +1,32 @@
+﻿using Chen_Xuxian_HW6.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chen_Xuxian_HW6.Utilities
+{
+    public class GenerateSKU
+    {
+        public static Int32 GetNextSKU(AppDbContext db)
+        {
+            Int32 intMaxSKU; //the current maximum course number
+            Int32 intNextSKU; //the course number for the next class
+
+            if (db.Products.Count() == 0)
+            {
+                intMaxSKU = 5000; 
+            }
+            else
+            {
+                intMaxSKU = db.Products.Max(c => c.SKU); 
+            }
+
+            //add one to the current max to find the next one
+            intNextSKU = intMaxSKU + 1;
+
+            //return the value
+            return intNextSKU;
+        }
+    }
+}
