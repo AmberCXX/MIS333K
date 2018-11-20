bd35ac9c436dc2c9c104fbbb5bc4eef8b0f87855
Xuxian Chen <amberwithmrtraveling@gmail.com>
2018-11-20 09:57:23 -0600

    Add files via upload

---
+++ Chen_Xuxian_HW3/Chen_Xuxian_HW3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chen_Xuxian_HW3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Chen_Xuxian_HW3/Chen_Xuxian_HW3/Models/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chen_Xuxian_HW3.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+
+}
